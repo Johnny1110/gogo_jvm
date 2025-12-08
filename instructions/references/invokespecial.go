4478b4f61b85989def322c363bb99a76a9011c06
Johnny.Wang <johnny.wang@btse.com>
2025-12-08 19:07:11 +0800

    docs: add invokespecial & invokevitual inst

--- instructions/references/invokespecial.go
+++ instructions/references/invokespecial.go
@@ -1,1 +1,25 @@
 package references
+
+import (
+	"github.com/Johnny1110/gogo_jvm/instructions/base"
+	"github.com/Johnny1110/gogo_jvm/runtime"
+)
+
+// INVOKESPECIAL
+// 1. constructor <init>
+// 2. private method
+// 3. parent method (super.xxx())
+// opcode = 0xB7
+// operands: 2 bytes (constant pool index)
+// stack: [objectref, arg1, arg2, ...] → [...]
+type INVOKESPECIAL struct {
+	base.Index16Instruction
+}
+
+func (i *INVOKESPECIAL) Execute(frame *runtime.Frame) {
+	// TODO
+}
+
+func (i *INVOKESPECIAL) Opcode() uint8 {
+	return 0xB7
+}
--- instructions/references/invokevirtual.go
+++ instructions/references/invokevirtual.go
@@ -1,1 +1,27 @@
 package references
+
+import (
+	"github.com/Johnny1110/gogo_jvm/instructions/base"
+	"github.com/Johnny1110/gogo_jvm/runtime"
+)
+
+// INVOKEVIRTUAL
+// ex:
+//
+//	Animal a = new Dog();
+//	a.speak();  // call Dog.speak() not Animal.speak()
+//
+// opcode = 0xB6
+// operands: 2 bytes (constant pool index)
+// stack: [objectref, arg1, arg2, ...] → [...]
+type INVOKEVIRTUAL struct {
+	base.Index16Instruction
+}
+
+func (i *INVOKEVIRTUAL) Execute(frame *runtime.Frame) {
+	// TODO
+}
+
+func (i *INVOKEVIRTUAL) Opcode() uint8 {
+	return 0xB7
+}
